81dc071e508242568b76b2a521bae28ed9cdca4c
Hanane ikhelef <hananeikhelef@gmail.com>
2019-07-07 15:00:17 -0400

    Add counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,14 +6,25 @@ class Counter extends Component {
 		tags: [ 'tag1', 'tag2', 'tag3' ]
 		//imageUrl: 'https://picsum.photos/200'
 	};
+	// this is for every event handeler but still we can ot use the constructor because it can be replaces by the arrow function
+	// constructor() {
+	// 	super();
+	// 	this.handleIncrement = this.handleIncrement.bind(this);
+	// }
 
-	handleIncrement() {
-		console.log('Increment handeled ');
-	}
+	handleIncrement = () => {
+		console.log('Increment handeled ', this);
+		this.state.count++;
+		this.setState({ count: this.state.count + 1 });
+	};
 	render() {
 		return (
 			<div>
 				<span className={this.getBadgeClasses()}> {this.formatCount()}</span>
+				{/* <button>
+					onClick= {this.handleIncrement} // not necessary but still helpful
+					className="btn btn-secondarybtn-sm"
+				</button> */}
 				<button className="btn btn-secondary btn-sm m-2">Increment</button>
 				<ul>{}</ul>
 			</div>
